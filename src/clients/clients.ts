6e80b1eef89084b878f6cf4bb0617e64d3bb269c
Denis Pop <pop.denis1902@gmail.com>
2022-07-12 15:37:39 +0300

    fix(clients-response): fixed clients response return type

--- src/clients/clients.ts
+++ src/clients/clients.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from "../http-client";
+import { Response } from "../response";
 import { Client } from "./types/client.type";
 
 export class Clients {
@@ -6,11 +7,11 @@ export class Clients {
 
   constructor(private readonly httpClient: HttpClient) {}
 
-  async list(): Promise<Client[] | undefined> {
+  async list(): Promise<Response<Client[] | undefined>> {
     return this.httpClient.get<Client[] | undefined>(this.#path);
   }
 
-  async getById(clientId: string): Promise<Client | undefined> {
+  async getById(clientId: string): Promise<Response<Client | undefined>> {
     return this.httpClient.get<Client | undefined>(`${this.#path}/${clientId}`);
   }
 }
